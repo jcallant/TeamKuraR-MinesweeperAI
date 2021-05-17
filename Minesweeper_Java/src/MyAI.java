aa77bf7e5b7c58edae76853a153c311abbdd241d
jrsca <jcallant@uci.edu>
2021-05-17 07:00:00 -0700

    testing

--- Minesweeper_Java/src/MyAI.java
+++ Minesweeper_Java/src/MyAI.java
@@ -436,11 +436,11 @@ public class MyAI extends AI {
 		Action a = coveredFrontier.stream()
 				.filter(t -> probability.containsKey(key(t.x,t.y)))
 				.filter(t -> {
-					boolean right = (t.x != COL_DIMENSIONS  && records.containsKey(key(t.x+1, t.y)) && records.get(key(t.x+1, t.y)) == -1);
+					boolean right = (t.x != COL_DIMENSIONS && records.containsKey(key(t.x+1, t.y)) && records.get(key(t.x+1, t.y)) == -1);
 					boolean left = (t.x != 1 && records.containsKey(key(t.x-1, t.y)) &&records.get(key(t.x-1, t.y)) == -1);
 					boolean top = (t.y != ROW_DIMENSIONS && records.containsKey(key(t.x, t.y+1)) && records.get(key(t.x, t.y+1)) == -1);
 					boolean bottom = (t.y != 1 && records.containsKey(key(t.x,t.y-1)) && records.get(key(t.x,t.y-1)) == -1);
-					return !((top && right) || (top && left) || (bottom && right) || (bottom && left));
+					return ((top && right) || (top && left) || (bottom && right) || (bottom && left));
 				})
 				.min(Comparator.comparing(t -> probability.get(key(t.x, t.y))))
 				.orElse(null);
