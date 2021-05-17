254e259774c41841733e783b4cb283b7203f9ca7
jrsca <jcallant@uci.edu>
2021-05-17 06:53:51 -0700

    testing

--- Minesweeper_Java/src/MyAI.java
+++ Minesweeper_Java/src/MyAI.java
@@ -429,10 +429,22 @@ public class MyAI extends AI {
 	private Action handleProbability(){
 		System.out.println("\nPicking from cf with lowest probability...");
 		System.out.println("probability: " + probability);
+//		Action a = coveredFrontier.stream()
+//				.filter(t -> probability.containsKey(key(t.x,t.y)))
+//				.min(Comparator.comparing(t -> probability.get(key(t.x, t.y))))
+//				.orElse(null);
 		Action a = coveredFrontier.stream()
 				.filter(t -> probability.containsKey(key(t.x,t.y)))
+				.filter(t -> {
+					boolean right = (t.x != COL_DIMENSIONS  && records.containsKey(key(t.x+1, t.y)) && records.get(key(t.x+1, t.y)) == -1);
+					boolean left = (t.x != 1 && records.containsKey(key(t.x-1, t.y)) &&records.get(key(t.x-1, t.y)) == -1);
+					boolean top = (t.y != ROW_DIMENSIONS && records.containsKey(key(t.x, t.y+1)) && records.get(key(t.x, t.y+1)) == -1);
+					boolean bottom = (t.y != 1 && records.containsKey(key(t.x,t.y-1)) && records.get(key(t.x,t.y-1)) == -1);
+					return !((top && right) || (top && left) || (bottom && right) || (bottom && left));
+				})
 				.min(Comparator.comparing(t -> probability.get(key(t.x, t.y))))
 				.orElse(null);
+
 		if(a!=null) {
 			coveredFrontier.remove(a);
 			currX = a.x;
