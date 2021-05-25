d899d0747af978357ca19695562f4e0863aee644
jrsca <jcallant@uci.edu>
2021-05-25 07:01:57 -0700

    testing

--- Minesweeper_Java/src/MyAI.java
+++ Minesweeper_Java/src/MyAI.java
@@ -501,6 +501,7 @@ public class MyAI extends AI {
 		ArrayList<HashMap<String,Integer>> possibleWorlds = new ArrayList<>();
 
 		for(int i=0; i<coveredFrontier.size(); i++){
+			System.out.printf("\ni=%d: %s\n",i, coveredFrontier);
 			ArrayList<Action> copy = coveredFrontier.stream()
 					.skip(i)
 					.collect(Collectors.toCollection(ArrayList::new));
@@ -542,7 +543,7 @@ public class MyAI extends AI {
 		Action a = frontier.remove(0);
 		int x = a.x;
 		int y = a.y;
-		//System.out.println(" hypoFlag: " + key(x,y));
+		System.out.println(" hypoFlag: " + key(x,y));
 
 		// if already marked safe, then can't be flagged as mine
 		if(hypoRecords.containsKey(key(x,y)) && hypoRecords.get(key(x,y)) == SAFE)
@@ -568,9 +569,9 @@ public class MyAI extends AI {
 				if (hypoRecords.containsKey(k) && hypoRecords.get(k) >= 0) {
 					int labelValue = hypoRecords.get(k);
 					labelValue--;
-					//System.out.println(String.format(" -label update: %s %d -> %d",k,labelValue+1, labelValue));
+					System.out.println(String.format(" -label update: %s %d -> %d",k,labelValue+1, labelValue));
 					if (labelValue == -1){
-						//System.out.println(" -label conflict: " + k);
+						System.out.println(" -label conflict: " + k);
 						return null; // if flagging as mine causes label conflict, then not possible
 					}
 					hypoRecords.put(k, labelValue);
@@ -583,9 +584,9 @@ public class MyAI extends AI {
 				else if (records.containsKey(k) && records.get(k) >= 0){
 					int labelValue = records.get(k);
 					labelValue--;
-					//System.out.println(String.format(" -label update: %s %d -> %d",k,labelValue+1, labelValue));
+					System.out.println(String.format(" -label update: %s %d -> %d",k,labelValue+1, labelValue));
 					if (labelValue == -1) {
-						//System.out.println(" -label conflict: " + k);
+						System.out.println(" -label conflict: " + k);
 						return null; // if flagging as mine causes label conflict, then not possible
 					}
 					hypoRecords.put(k, labelValue);
@@ -597,27 +598,27 @@ public class MyAI extends AI {
 				}
 			}
 		}
-		//System.out.println(" hypoRecord: " + hypoRecords);
+		System.out.println(" hypoRecord: " + hypoRecords);
 
-		//System.out.print(" >> checking if valid...");
+		System.out.print(" >>>>>>> checking if valid...");
 		boolean valid = true;
 		for (Action action : uncoveredFrontier) {
 			String k = key(action.x, action.y);
 			if (!hypoRecords.containsKey(k) || hypoRecords.get(k) > 0) {
-				//System.out.println("N: unsatisfied label " + k);
+				System.out.println("N: unsatisfied label " + k);
 				valid = false;
 				break;
 			}
 		}
 		if(valid) {
-			//System.out.println("Y: possible world found");
+			System.out.println("Y: possible world found\n");
 			return hypoRecords;
 		}
 		else if (!frontier.isEmpty()) {
 			while (hypoFlagAndUpdate(frontier, hypoRecords) == null) {
-				//System.out.println(" -not possible. trying next.");
+				System.out.println(" -not possible. trying next.");
 				if (frontier.isEmpty()) {
-					//System.out.println(" -list empty. cascading.");
+					System.out.println(" -list empty. cascading.");
 					return null;
 				}
 			}
