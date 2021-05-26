0483b862299142eb91a7c6414455ac1d3fe909aa
jrsca <jcallant@uci.edu>
2021-05-26 00:34:57 -0700

    testing

--- Minesweeper_Java/src/MyAI.java
+++ Minesweeper_Java/src/MyAI.java
@@ -608,7 +608,6 @@ public class MyAI extends AI {
 	private HashMap<String, Integer> hypoFlagAndUpdate(ArrayList<Action> frontier, HashMap<String, Integer> hypoRecords) {
 
 		for(Action a : frontier) {
-			a = frontier.remove(0);
 			int x = a.x;
 			int y = a.y;
 //		//System.out.println(" <hypoFlag: " + key(x, y));
